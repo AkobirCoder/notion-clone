07e7cf448edabcb7807d83d6af36df36dddb0a1b
AkobirCoder <akobir0308@gmail.com>
2026-04-05 23:57:16 +0500

    navbar.tsx was added to app/(home)/_components/

---
+++ app/(home)/_components/navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Logo } from './logo';
+
+export const Navbar = () => {
+    return (
+        <div className='z-50 bg-background fixed top-0 flex items-center w-full p-6'>
+            <Logo />
+            <div className='flex items-center gap-x-2'>
+                
+            </div>
+        </div>
+    );
+}
