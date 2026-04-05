e49ae29417510a5b1e8265c6594b2118440649b9
AkobirCoder <akobir0308@gmail.com>
2026-04-05 23:55:14 +0500

    layout.tsx was added to app/(home)/

---
+++ app/(home)/layout.tsx
@@ -0,0 +1,17 @@
+import { ChildProps } from '@/types';
+import React from 'react';
+import { Navbar } from './_components';
+
+
+const HomeLayout = ({children}: ChildProps) => {
+    return (
+        <div className='h-full'>
+            <Navbar />
+            <main className='h-full pt-40'>
+                {children}
+            </main>
+        </div>
+    );
+}
+
+export default HomeLayout;
