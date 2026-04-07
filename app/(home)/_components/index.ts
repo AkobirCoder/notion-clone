cccb5088e9698044c41875a02307fd46507742a7
AkobirCoder <akobir0308@gmail.com>
2026-04-07 23:53:21 +0500

    app/(home)/_components/index.ts was updated

--- app/(home)/_components/index.ts
+++ app/(home)/_components/index.ts
@@ -1,3 +1,8 @@
+import { Footer } from './footer';
+import { PricingCard } from './pricing-card';
+import { Pricing } from './pricing';
+import { Clients } from './clients';
+import { Heroes } from './heroes';
 import { Navbar } from "./navbar";
 
-export { Navbar };
+export { Navbar, Heroes, Clients, Pricing, PricingCard, Footer };
