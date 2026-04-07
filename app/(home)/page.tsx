4847e92765c1f16b2caf26ab3f5a4a1579c747f0
AkobirCoder <akobir0308@gmail.com>
2026-04-07 23:51:23 +0500

    app/(home)/page.tsx was updated

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,14 @@
+import { Clients, Footer, Heroes, Pricing } from "./_components";
+
 export default function Home() {
     return (
-        <div>hello</div>
+        <div className="min-h-full flex flex-col">
+            <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+                <Heroes />
+                <Clients />
+            </div>
+            <Pricing />
+            <Footer />
+        </div>
     );
 }
