c701a6056ab927e15cc821573ed7dd6af5a0ae8c
AkobirCoder <akobir0308@gmail.com>
2026-04-09 01:14:57 +0500

    app/layout.tsx was updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Roboto } from "next/font/google";
 import "./globals.css";
 import { Metadata } from "next";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ConvexClientProvider } from "@/components/providers/convex-client-provider";
 
 const roboto = Roboto({
     subsets: ["latin"],
@@ -25,15 +26,17 @@ export default function RootLayout({
     return (
         <html lang="en" className={`${roboto.variable} h-full`} suppressHydrationWarning>
             <body suppressHydrationWarning>
-                <ThemeProvider
-                    attribute="class"
-                    defaultTheme="system"
-                    enableSystem
-                    disableTransitionOnChange
-                    storageKey="notion-theme"
-                >
-                    {children}
-                </ThemeProvider>
+                <ConvexClientProvider>
+                    <ThemeProvider
+                        attribute="class"
+                        defaultTheme="system"
+                        enableSystem
+                        disableTransitionOnChange
+                        storageKey="notion-theme"
+                    >
+                        {children}
+                    </ThemeProvider>
+                </ConvexClientProvider>
             </body>
         </html>
     );
