b8e07694ae6eeee4dc615e05a75ad8769f5328ed
AkobirCoder <akobir0308@gmail.com>
2026-04-09 01:15:43 +0500

    app/(home)/_components/navbar.tsx was updated

--- app/(home)/_components/navbar.tsx
+++ app/(home)/_components/navbar.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client"
 
 import React from 'react';
 import { Logo } from './logo';
@@ -6,8 +6,14 @@ import { ModeToggle } from '@/components/shared/mode-toggle';
 import { Button } from '@/components/ui/button';
 import { useScrolled } from '@/hooks/use-scrolled';
 import { cn } from '@/lib/utils';
+import { SignInButton, UserButton } from '@clerk/clerk-react';
+import { useConvexAuth } from 'convex/react';
+import Link from 'next/link';
+import { Spinner } from '@/components/ui/spinner';
 
 export const Navbar = () => {
+    const {isAuthenticated, isLoading} = useConvexAuth();
+
     const scrolled = useScrolled();
 
     return (
@@ -18,12 +24,40 @@ export const Navbar = () => {
         >
             <Logo />
             <div className='flex items-center gap-x-2'>
-                <Button size={'sm'} variant={'outline'}>
-                    Sign In
-                </Button>
-                <Button size={'sm'}>
-                    Get Notion Free
-                </Button>
+                {
+                    isLoading && <Spinner />
+                }
+
+                {
+                    !isAuthenticated && !isLoading && (
+                        <>
+                            <SignInButton mode="modal">
+                                <Button size={'sm'} variant={'outline'}>
+                                    Sign In
+                                </Button>
+                            </SignInButton>
+                            <SignInButton mode="modal">
+                                <Button size={'sm'}>
+                                    Get Notion Free
+                                </Button>
+                            </SignInButton>
+                        </>
+                    )
+                }
+
+                {
+                    isAuthenticated && !isLoading && (
+                        <>
+                            <Button variant={'outline'} size={'sm'} asChild>
+                                <Link href={'/documents'}>
+                                    Enter notion
+                                </Link>
+                            </Button>
+                            <UserButton afterSignOutUrl='/' />
+                        </>
+                    )
+                }
+
                 <ModeToggle />
             </div>
         </div>
