2bb6be0ef3349dad13564d905afd5b33baa3e4a7
AkobirCoder <akobir0308@gmail.com>
2026-06-10 21:44:19 +0500

    app/(secret)/_components/document-list.tsx was updated

--- app/(secret)/_components/document-list.tsx
+++ app/(secret)/_components/document-list.tsx
@@ -5,6 +5,8 @@ import { Id } from '@/convex/_generated/dataModel';
 import { useQuery } from 'convex/react';
 import React, { useState } from 'react';
 import { Item } from './item';
+import { cn } from '@/lib/utils';
+import { useRouter } from 'next/navigation';
 
 interface DocumentListProps {
     parentDocumentId?: Id<"documents">;
@@ -13,6 +15,8 @@ interface DocumentListProps {
 
 export const DocumentList = ({parentDocumentId, level = 0}: DocumentListProps) => {
     const [expanded, setExpanded] = useState<Record<string, boolean>>({});
+
+    const router = useRouter();
 
     const onExpand = (documentId: string) => {
         setExpanded((prevState) => {
@@ -23,16 +27,49 @@ export const DocumentList = ({parentDocumentId, level = 0}: DocumentListProps) =
         });
     }
 
+    const onRedirect = (documentId: string) => {
+        router.push(`/documents/${documentId}`);
+    }
+
     const documents = useQuery(api.document.getDocuments, {
         parentDocument: parentDocumentId,
     });
 
     console.log(documents);
 
+    if (documents === undefined) {
+        return (
+            <>
+                <Item.Skeleton level={level} />
+
+                {
+                    level === 0 && (
+                        <>
+                            <Item.Skeleton level={level} />
+                            <Item.Skeleton level={level} />
+                        </>
+                    )
+                }
+            </>
+        );
+    }
+
     return (
         <>
+            <p 
+                className={
+                    cn("hidden text-sm font-medium text-muted-foreground/80", 
+                    expanded && "last:block",
+                    level === 0 && "hidden")
+                }
+                style={{
+                    paddingLeft: level ? `${level * 12 + 25}px` : undefined
+                }}
+            >
+                No documents found.
+            </p>
             {
-                documents?.map((document) => {
+                documents.map((document) => {
                     return (
                         <div 
                             key={document._id}
@@ -43,6 +80,7 @@ export const DocumentList = ({parentDocumentId, level = 0}: DocumentListProps) =
                                 level={level}
                                 expanded={expanded[document._id]}
                                 onExpand={() => onExpand(document._id)}
+                                onRedirect={() => onRedirect(document._id)}
                             />
                             {
                                 expanded[document._id] && (
