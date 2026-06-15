da237ad24f91799189fa72e38f8dac7ac93b1b65
AkobirCoder <akobir0308@gmail.com>
2026-06-15 23:31:57 +0500

    navbar.tsx was added to app/(secret)/_components/

---
+++ app/(secret)/_components/navbar.tsx
@@ -0,0 +1,58 @@
+import { Spinner } from '@/components/ui/spinner';
+import { api } from '@/convex/_generated/api';
+import { Id } from '@/convex/_generated/dataModel';
+import { useQuery } from 'convex/react';
+import { MenuIcon } from 'lucide-react';
+import { useParams } from 'next/navigation';
+import React from 'react';
+import { Title } from './title';
+
+interface NavbarProps {
+    isCollapsed: boolean,
+    reset: () => void,
+}
+
+export const Navbar = ({isCollapsed, reset}: NavbarProps) => {
+    const params = useParams();
+
+    const document = useQuery(api.document.getDocumentsById, {
+        id: params.documentId as Id<"documents">,
+    });
+
+    if (document === undefined) {
+        return (
+            <nav className='flex items-center justify-between w-full px-3 py-2 bg-background'>
+                <Title.Skeleton />
+                <div className='flex items-center gap-x-2'>
+                    <Spinner />
+                </div>
+            </nav>
+        );
+    }
+
+    if (document === null) {
+        return null;
+    }
+
+    return (
+        <>
+            <nav className='flex items-center gap-x-4 w-full px-3 py-2 bg-background'>
+                {
+                    isCollapsed && (
+                        <MenuIcon
+                            className='h-6 w-6 text-muted-foreground cursor-pointer' 
+                            role="button"
+                            onClick={reset}
+                        />
+                    )
+                }
+                <div className='flex items-center justify-between w-full'>
+                    <Title document={document} />
+                    <div className='flex items-center gap-x-2'>
+
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
+}
