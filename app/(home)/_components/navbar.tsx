94f525c3a5cd8356298f16c2b6480e95e2840b5c
AkobirCoder <akobir0308@gmail.com>
2026-06-18 23:30:38 +0500

    app/(home)/_components/navbar.tsx was updated

--- app/(home)/_components/navbar.tsx
+++ app/(home)/_components/navbar.tsx
@@ -4,12 +4,12 @@ import React from 'react';
 import { Logo } from './logo';
 import { ModeToggle } from '@/components/shared/mode-toggle';
 import { Button } from '@/components/ui/button';
-import { useScrolled } from '@/hooks/use-scrolled';
 import { cn } from '@/lib/utils';
 import { SignInButton, UserButton } from '@clerk/clerk-react';
 import { useConvexAuth } from 'convex/react';
 import Link from 'next/link';
 import { Spinner } from '@/components/ui/spinner';
+import { useScrolled } from '@/hooks/use-scrolled';
 
 export const Navbar = () => {
     const {isAuthenticated, isLoading} = useConvexAuth();
@@ -57,7 +57,6 @@ export const Navbar = () => {
                         </>
                     )
                 }
-
                 <ModeToggle />
             </div>
         </div>
