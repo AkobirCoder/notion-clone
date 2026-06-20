71d64bc93f6945f20ebbef0c09b96ca672a3d916
AkobirCoder <akobir0308@gmail.com>
2026-06-20 23:30:10 +0500

    editor.tsx was added to components/shared/

---
+++ components/shared/editor.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import "@blocknote/core/fonts/inter.css";
+import "@blocknote/mantine/style.css";
+import { useTheme } from 'next-themes';
+import React from 'react';
+import { BlockNoteView } from '@blocknote/mantine';
+// import { BlockNoteEditor, PartialBlock } from '@blocknote/core';
+import { useCreateBlockNote } from '@blocknote/react';
+
+interface EditorProps {
+    initialContent?: string,
+    editable?: boolean,
+    onChange: (value: string) => void,
+}
+
+const Editor = ({initialContent, editable, onChange}: EditorProps) => {
+    const {resolvedTheme} = useTheme();
+
+    const editor = useCreateBlockNote({
+        editable,
+        initialContent: initialContent
+            ? JSON.parse(initialContent)
+            : undefined,
+    });
+
+    return (
+        <BlockNoteView 
+            editor={editor} 
+            theme={resolvedTheme === "dark" ? "dark" : "light"}
+            onChange={() => {
+                // console.log("editor changed");
+
+                onChange(JSON.stringify(editor.document));
+            }}
+        />
+    );
+}
+
+export default Editor;
