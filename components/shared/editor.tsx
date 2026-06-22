26af919061f26db459ae59a5f44f1a736efe0953
AkobirCoder <akobir0308@gmail.com>
2026-06-23 00:54:13 +0500

    components/shared/editor.tsx was updated

--- components/shared/editor.tsx
+++ components/shared/editor.tsx
@@ -7,26 +7,40 @@ import React from 'react';
 import { BlockNoteView } from '@blocknote/mantine';
 // import { BlockNoteEditor, PartialBlock } from '@blocknote/core';
 import { useCreateBlockNote } from '@blocknote/react';
+import { useEdgeStore } from "@/lib/edgestore";
+import { cn } from "@/lib/utils";
 
 interface EditorProps {
+    preview?: boolean,
     initialContent?: string,
     editable?: boolean,
     onChange: (value: string) => void,
 }
 
-const Editor = ({initialContent, editable, onChange}: EditorProps) => {
-    const {resolvedTheme} = useTheme();
+const Editor = ({preview, initialContent, editable, onChange}: EditorProps) => {
+    const { resolvedTheme } = useTheme();
+
+    const { edgestore } = useEdgeStore();
+
+    const handleUpload = async (file: File) => {
+        const res = await edgestore.publicFiles.upload({file});
+
+        return res.url;
+    }
 
     const editor = useCreateBlockNote({
         editable,
         initialContent: initialContent
             ? JSON.parse(initialContent)
             : undefined,
+        uploadFile: handleUpload,
     });
 
     return (
         <BlockNoteView
+            className={cn('mt-15', preview && 'mt-0')}
             editor={editor}
+            editable={editable}
             theme={resolvedTheme === "dark" ? "dark" : "light"}
             onChange={() => {
                 // console.log("editor changed");
