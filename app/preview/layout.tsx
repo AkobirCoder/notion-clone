45a9e70d1966d25570881654c6d07f165a4de8c3
AkobirCoder <akobir0308@gmail.com>
2026-06-23 00:51:10 +0500

    layout.tsx was added to app/preview/

---
+++ app/preview/layout.tsx
@@ -0,0 +1,12 @@
+import { ChildProps } from '@/types';
+import React from 'react';
+
+const Layout = ({children}: ChildProps) => {
+    return (
+        <div className='h-full'>
+            {children}
+        </div>
+    );
+}
+
+export default Layout;
