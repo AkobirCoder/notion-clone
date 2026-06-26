b8ed175b063151ae0c63d5d87df9e0f9fa660b44
AkobirCoder <akobir0308@gmail.com>
2026-06-26 18:38:48 +0500

    route.ts was added to app/api/stripe/subscription/

---
+++ app/api/stripe/subscription/route.ts
@@ -0,0 +1,62 @@
+import stripe from "@/lib/stripe";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+    try {
+        const public_domain = process.env.NEXT_PUBLIC_DOMAIN;
+
+        const {email, userId, priceId} = await req.json();
+
+        const isExistingCustomer = await stripe.customers.list({email});
+
+        let customer;
+
+        if (isExistingCustomer.data.length) {
+            customer = isExistingCustomer.data[0];
+        }
+
+        if (!customer) {
+            customer = await stripe.customers.create({
+                email,
+                metadata: {
+                    userId
+                },
+            });
+        }
+
+        const subscriptions = await stripe.subscriptions.list({
+            customer: customer.id,
+        });
+
+        const isSubscribed = subscriptions.data.find((subscription) => {
+            return subscription.status === "active";
+        });
+
+        if (!isSubscribed) {
+            const subscription = await stripe.checkout.sessions.create({
+                mode: "subscription",
+                payment_method_types: ["card"],
+                line_items: [{
+                    price: priceId,
+                    quantity: 1,
+                }],
+                customer: customer.id,
+                success_url: `${public_domain}/documents?`,
+                cancel_url: `${public_domain}`,
+            });
+
+            return NextResponse.json(subscription.url);
+        } else {
+            const portal = await stripe.billingPortal.sessions.create({
+                customer: customer.id,
+                return_url: `${public_domain}/documents`,
+            });
+
+            return NextResponse.json(portal.url);
+        }   
+    } catch (error) {
+        return NextResponse.json(`Something went wrong. Please try again. - ${error}`, {
+            status: 500,
+        });
+    }
+}
