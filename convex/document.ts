19c324e34bc0fda0110748f0b2de22a88d32c853
AkobirCoder <akobir0308@gmail.com>
2026-06-27 23:10:08 +0500

    convex/document.ts was updated

--- convex/document.ts
+++ convex/document.ts
@@ -315,3 +315,27 @@ export const getSearchDocuments = query({
         return documents;
     }
 });
+
+export const getAllDocuments = query({
+    handler: async (ctx) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if (!identity) {
+            throw new Error("Not authenticated");
+        }
+
+        const userId = identity.subject;
+
+        const documents = await ctx.db
+            .query("documents")
+            .withIndex(
+                "by_user",
+                (q) => q.eq("userId", userId)
+            )
+            .filter(q => q.eq(q.field("isArchived"), false))
+            .order("desc")
+            .collect();
+
+        return documents;
+    },
+});
